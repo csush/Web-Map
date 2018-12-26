32f8606c04b79162e7a6fea4aaf43f0f37070678
Sushant Chaudhary <csush.95@gmail.com>
2018-12-25 20:06:41 -0800

    added markers

--- webmap.py
+++ webmap.py
@@ -2,7 +2,10 @@
 import folium
 
 #Create base map
-map = folium.Map(location=[37.296933,-121.9574983], zoom_start = 8)
+map = folium.Map(location=[37.296933,-121.9574983], zoom_start = 8, tiles ="Mapbox bright")
+
+for coordinate in [[37.4074687,-122.086669], [37.8199286,-122.4804438]]:
+	folium.Marker(location=coordinate, icon=folium.Icon(color = 'green')).add_to(map)
 
 #Save the map
 map.save("map1.html")
