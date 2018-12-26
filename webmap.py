ef314706b7b39669f17f1615b280929d00d0c082
Sushant Chaudhary <csush.95@gmail.com>
2018-12-25 20:24:08 -0800

    display volcanoes with elevation

--- webmap.py
+++ webmap.py
@@ -1,11 +1,17 @@
-#Import Library
 import folium
 
-#Create base map
-map = folium.Map(location=[37.296933,-121.9574983], zoom_start = 8, tiles ="Mapbox bright")
+import pandas as pd
 
-for coordinate in [[37.4074687,-122.086669], [37.8199286,-122.4804438]]:
-	folium.Marker(location=coordinate, icon=folium.Icon(color = 'green')).add_to(map)
+data = pd.read_csv("us-volcanoes.txt")
+Lat = data['LAT']
+Lon = data['LON']
+Elevation = data['ELEV']
+
+#Create base map
+map = folium.Map(location=[37.296933,-121.9574983], zoom_start = 5, tiles ="Mapbox bright")
+
+for lat, lon, elevation in zip(Lat, Lon, Elevation):
+	folium.Marker(location=[lat, lon], popup=str(elevation) + " m", icon=folium.Icon(color = 'red')).add_to(map)
 
 #Save the map
 map.save("map1.html")
