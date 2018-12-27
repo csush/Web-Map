2dbb21dea1d803038129886e091fdda2e1cce852
Sushant Chaudhary <csush.95@gmail.com>
2018-12-27 14:00:56 -0800

    simplified markers, made clusters

--- webmap.py
+++ webmap.py
@@ -1,17 +1,30 @@
 import folium
+from folium.plugins import MarkerCluster
 
 import pandas as pd
 
+#Loading data as dataframes using pandas
 data = pd.read_csv("us-volcanoes.txt")
 Lat = data['LAT']
 Lon = data['LON']
 Elevation = data['ELEV']
 
+#Color corresponding to elevation of volcanoes
+def switch_color(elevation):
+	if (elevation < 1000):
+		return ('green')
+	elif (1000 <= elevation < 3000):
+		return ('orange')
+	else: return ('red')
+
 #Create base map
-map = folium.Map(location=[37.296933,-121.9574983], zoom_start = 5, tiles ="Mapbox bright")
+map = folium.Map(location=[37.296933,-121.9574983], zoom_start = 5, tiles ="CartoDB dark_matter")
+
+#Create cluster
+marker_cluster = MarkerCluster().add_to(map)
 
 for lat, lon, elevation in zip(Lat, Lon, Elevation):
-	folium.Marker(location=[lat, lon], popup=str(elevation) + " m", icon=folium.Icon(color = 'red')).add_to(map)
+	folium.CircleMarker(location=[lat, lon], radius=9, popup=str(elevation) + " m", fill_color=switch_color(elevation), color="gray", fill_opacity=0.9).add_to(marker_cluster)
 
 #Save the map
 map.save("map1.html")
